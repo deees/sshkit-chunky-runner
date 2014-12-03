14843de1402d2fa20867f11e5562e6f09d6cbff1
Tomas Brazys <tomas.brazys@gmail.com>
2014-12-03 10:21:45 +0200

    Fix implementation of chunking

--- lib/sshkit/chunky/runner/chunks.rb
+++ lib/sshkit/chunky/runner/chunks.rb
@@ -1,13 +1,34 @@
 module SSHKit
   module Chunky
     module Runner
-      class Chunks < SSHKit::Runner::Group
+      class Chunks < SSHKit::Runner::Sequential
         attr_writer :chunks_count
+
+        def execute
+          each_chunk(hosts).map do |group_hosts|
+            SSHKit::Runner::Parallel.new(group_hosts, &block).execute
+            sleep wait_interval
+          end.flatten
+        end
 
         private
 
-        def group_size
-          (hosts.size.to_f / chunks_count).ceil
+        def each_chunk(array)
+          return array.map { |item| [item] } if array.size <= chunks_count
+
+          division = array.size.div chunks_count
+          modulo = array.size % chunks_count
+
+          groups = []
+          start = 0
+
+          chunks_count.times do |index|
+            length = division + (modulo > 0 && modulo > index ? 1 : 0)
+            groups << array.slice(start, length)
+            start += length
+          end
+
+          groups
         end
 
         def chunks_count
--- spec/lib/sshkit/chunky/runner/chunks_spec.rb
+++ spec/lib/sshkit/chunky/runner/chunks_spec.rb
@@ -1,36 +1,70 @@
 require 'sshkit/chunky/runner/chunks'
 
 describe SSHKit::Chunky::Runner::Chunks do
-  describe '#group_size' do
-    let(:options) { { count: 3 } }
+  describe '#each_chunk' do
+    let(:options) { { count: 4, sleep: 0 } }
     let(:block_to_run) { ->(host) { execute "echo #{Time.now.to_i}" } }
     let(:runner) do
       SSHKit::Chunky::Runner::Chunks.new(hosts, options, &block_to_run)
     end
 
-    subject(:group_size) { runner.send(:group_size) }
+    subject(:each_chunk) { runner.send(:each_chunk, hosts) }
 
     context 'with one host' do
       let(:hosts) { ['example.com'] }
 
-      it 'returns one' do
-        expect(group_size).to eq 1
+      it 'returns 1 group' do
+        expect(each_chunk.size).to eq 1
+      end
+
+      it 'returns 1 item in first group' do
+        expect(each_chunk.first.size).to eq 1
       end
     end
 
-    context 'with 6 hosts' do
-      let(:hosts) { ['example.com'] * 6 }
+    context 'with 3 hosts' do
+      let(:hosts) { ['example.com'] * 3 }
 
-      it 'returns 2' do
-        expect(group_size).to eq 2
+      it 'returns 3 groups' do
+        expect(each_chunk.size).to eq 3
+      end
+
+      it 'returns 1 item in each group' do
+        expect(each_chunk[0].size).to eq 1
+        expect(each_chunk[1].size).to eq 1
+        expect(each_chunk[2].size).to eq 1
       end
     end
 
-    context 'with 7 hosts' do
-      let(:hosts) { ['example.com'] * 7 }
+    context 'with 8 hosts' do
+      let(:hosts) { ['example.com'] * 8 }
 
-      it 'returns 3' do
-        expect(group_size).to eq 3
+      it 'returns 4 groups' do
+        expect(each_chunk.size).to eq 4
+      end
+      it 'returns 2 items in each group' do
+        expect(each_chunk[0].size).to eq 2
+        expect(each_chunk[1].size).to eq 2
+        expect(each_chunk[2].size).to eq 2
+        expect(each_chunk[3].size).to eq 2
+      end
+    end
+
+    context 'with 9 hosts' do
+      let(:hosts) { ['example.com'] * 9 }
+
+      it 'returns 4 groups' do
+        expect(each_chunk.size).to eq 4
+      end
+
+      it 'returns 3 items in first group' do
+        expect(each_chunk[0].size).to eq 3
+      end
+
+      it 'returns 2 items in other groups' do
+        expect(each_chunk[1].size).to eq 2
+        expect(each_chunk[2].size).to eq 2
+        expect(each_chunk[3].size).to eq 2
       end
     end
   end
